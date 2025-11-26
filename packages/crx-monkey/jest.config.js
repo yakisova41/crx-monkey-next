cdafbe0df9aeb464150897709bd0a16df66bd5b9
yakisova41 <yakisoba5emon@gmail.com>
2025-11-27 00:12:51 +0900

    buah!

--- packages/crx-monkey/jest.config.js
+++ packages/crx-monkey/jest.config.js
@@ -1,7 +1,3 @@
-import { createDefaultPreset } from 'ts-jest';
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 export default {
   preset: 'ts-jest/presets/default-esm',
