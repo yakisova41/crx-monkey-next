8a151542e21975cafe01ec314b6349e1c386cc5e
yakisova41 <yakisoba5emon@gmail.com>
2026-02-12 01:51:29 +0900

    Export plugins

--- packages/crx-monkey/src/node/exports.ts
+++ packages/crx-monkey/src/node/exports.ts
@@ -6,7 +6,9 @@ import {
 } from './typeDefs';
 import { tsBundler, tsBundlerWatch } from './plugins/tsBundler';
 import { sassBundler, sassBundlerWatch } from './plugins/sassBundler';
-import { htmlBundler, htmlBundlerWatch } from './plugins/htmlBundler/main';
+
+export { tsBundler, tsBundlerWatch };
+export { sassBundler, sassBundlerWatch };
 
 /**
  * Define config for crx monkey.
@@ -30,16 +32,6 @@ export function defineConfig(userConfig: CrxmConfig): CrxmConfigRequired {
     build: {
       '^.*.(ts|js|tsx|jsx)$': tsBundler({}),
       '^.*.(css|sass|scss)$': sassBundler(),
-      '^.*.(html|htm)$': htmlBundler({
-        output: 'dist/chrome',
-        plugins: {
-          build: { '^.*.(ts|js|tsx|jsx)$': tsBundler(), '^.*.(css|sass|scss)$': sassBundler() },
-          watch: {
-            '^.*.(ts|js|tsx|jsx)$': tsBundlerWatch(),
-            '^.*.(css|sass|scss)$': sassBundlerWatch(),
-          },
-        },
-      }),
     },
     watch: {
       '^.*.(ts|js|tsx|jsx)$': tsBundlerWatch({
@@ -48,16 +40,6 @@ export function defineConfig(userConfig: CrxmConfig): CrxmConfigRequired {
         },
       }),
       '^.*.(css|sass|scss)$': sassBundlerWatch(),
-      '^.*.(html|htm)$': htmlBundlerWatch({
-        output: 'dist/chrome',
-        plugins: {
-          build: { '^.*.(ts|js|tsx|jsx)$': tsBundler(), '^.*.(css|sass|scss)$': sassBundler() },
-          watch: {
-            '^.*.(ts|js|tsx|jsx)$': tsBundlerWatch(),
-            '^.*.(css|sass|scss)$': sassBundlerWatch(),
-          },
-        },
-      }),
     },
     logLevel: 'error',
     public: false,
